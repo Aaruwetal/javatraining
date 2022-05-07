d10bf3ed4ba1d64d5cfef3aa7c5bc349de2431e8
Aaruwetal <artiraosahebwetal28@gmail.com>
2022-05-07 15:50:59 +0530

    initial commit
    Day1

---
+++ 5 April/adition.java
@@ -0,0 +1,14 @@
+//separte digit and prnt addition unput a=8+9,
+import java.util.*;
+class concat
+{
+public static void main( String args[])
+{
+int a,b,c;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+c=a+b;
+System.out.println("Adedition is" +c);
+}
+}
